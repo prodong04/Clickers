1f008e5a97d93d1fb0ac0226f4a663054f3adfa1
prodong04 <dragon0401@naver.com>
2025-04-04 04:06:30 +0900

    Ignore __pycache__ and config folder

--- config/config_loader.py
+++
@@ -1,9 +0,0 @@
-import yaml
-
-def load_config(config_path: str = "config.yaml") -> dict:
-    """
-    config.yaml 파일을 불러와서 Python 딕셔너리로 반환합니다.
-    """
-    with open(config_path, "r", encoding="utf-8") as file:
-        config = yaml.safe_load(file)
-    return config
